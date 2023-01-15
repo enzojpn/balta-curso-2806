79f1f4a46fb5edc90d8d9ec9a0a0d523126b25aa
enzojpn <enzo_jpn@hotmail.com>
2023-01-15 17:24:12 -0300

    insert new data AWS

--- Models/Category.cs
+++ Models/Category.cs
@@ -2,8 +2,15 @@ namespace BaltaDataAcess.Models
 {
     public class Category
     {
-        public Guid Id { get; set; }    
+        public Guid Id { get; set; }
 
         public String Title { get; set; }
+
+        public String Url { get; set; }
+        public String Summary { get; set; }
+        public int Order { get; set; }
+
+        public String Description { get; set; }
+        public bool Featured { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -14,16 +14,41 @@ using (var conn = new SqlConnection(connectionString))
 {
     Console.WriteLine("conectado!");
     conn.Open();
+
+    var category = new Category();
+    category.Description = "Amazon AWS  sw";
+    category.Featured = false;
+    category.Id = Guid.NewGuid();
+    category.Order = 8;
+    category.Summary = "AWS amaxon";
+    category.Title = "Amazon server";
+    category.Url = "amazon";
+
+
+    var insertSql = "INSERT INTO [Category] values (@Id, @Title, @Url, @Summary , @Order ,@Description ,@Featured)";
+
     using (var comamnd = new SqlCommand())
     {
+
+      var rows =   conn.Execute(insertSql, new
+        {
+            category.Description,
+        category.Featured ,
+        category.Id ,
+        category.Order ,
+        category.Summary ,
+        category.Title ,
+        category.Url ,
+    });
+Console.WriteLine($"{rows} linhas inseridas ");
         var categories = conn.Query<Category>("Select [Id],[Title] from [Category]");
 
-        foreach (var categorie in categories)
-        {
-            Console.WriteLine($"{categorie.Id} - {categorie.Title}");
-        }
-
+    foreach (var categorie in categories)
+    {
+        Console.WriteLine($"{categorie.Id} - {categorie.Title}");
     }
+
+}
 }
 
 
